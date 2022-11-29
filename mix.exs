c61abb0635cc967118654ac0ec4cb736b8338d0f
Mayel de Borniol <git@mayel.space>
2022-11-30 12:00:20 +1300

    mix

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-Code.eval_file("mess.exs")
+Code.eval_file("mess.exs", (if File.exists?("../../lib/mix/mess.exs"), do: "../../lib/mix/"))
 
 defmodule Bonfire.TaxonomySeeder.MixProject do
   use Mix.Project
